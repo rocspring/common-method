7840c0d31eaeb5570afc160c17903566d8bb1178
wangshaopeng <wangshaopeng@panda.tv>
2018-05-09 15:06:29 +0800

    add throttle fun

--- time.js
+++ time.js
@@ -1,15 +1,14 @@
 var methods = {
-
   /**
    * @desc 随机延迟执行一个方法
    * @param maxTime {number} 延迟的最大时间(单位s)
    * @param method {function} 延迟执行的回调方法
    */
-  randomDelayExecuteMethod: function (maxTime, method) {
+  randomDelayExecuteMethod: function(maxTime, method) {
     maxTime = Number(maxTime) || 60;
     var randomNum = getRandomIntInclusive(0, maxTime);
 
-     setTimeout(function() {
+    setTimeout(function() {
       method && method();
     }, randomNum * 1000);
 
@@ -26,15 +25,16 @@ var methods = {
    * @param {string} format, 格式化需要的模版
    * @return {string} 格式化后的日期
    */
-  dateformat: function (timer, format) {
+  dateformat: function(timer, format) {
     timer = new Date(Number(timer));
     if (format) {
-        timer = format.replace("%Y", timer.getFullYear())
-            .replace("%m", formatter(timer.getMonth() + 1))
-            .replace("%d", formatter(timer.getDate()))
-            .replace("%H", formatter(timer.getHours()))
-            .replace("%M", formatter(timer.getMinutes()))
-            .replace("%S", formatter(timer.getSeconds()));
+      timer = format
+        .replace("%Y", timer.getFullYear())
+        .replace("%m", formatter(timer.getMonth() + 1))
+        .replace("%d", formatter(timer.getDate()))
+        .replace("%H", formatter(timer.getHours()))
+        .replace("%M", formatter(timer.getMinutes()))
+        .replace("%S", formatter(timer.getSeconds()));
     }
     return timer;
 
@@ -44,16 +44,46 @@ var methods = {
      * @return {number} 标准化后的数字
      */
     function formatter(n) {
-        if (typeof n === 'string') {
-            n = parseInt(n, 10);
-        }
+      if (typeof n === "string") {
+        n = parseInt(n, 10);
+      }
 
-        if (n < 10) {
-            return '0' + n;
-        }
-        return n;
+      if (n < 10) {
+        return "0" + n;
+      }
+      return n;
     }
-}
+  },
+
+    /**
+   * 函数节流函数
+   * @param  {Function} func  需要节流的目标函数
+   * @param  {?Object} wait 间隔时间
+   */
+  throttle: function(func, wait) {
+    var context, args, timeout, result, previous, later;
+    previous = 0;
+    later = function() {
+      previous = new Date();
+      timeout = null;
+      result = func.apply(context, args);
+    };
+    return function() {
+      var now = new Date(),
+        remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0) {
+        clearTimeout(timeout);
+        timeout = null;
+        previous = now;
+        result = func.apply(context, args);
+      } else if (!timeout) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  }
 };
 
 module.exports = methods;
